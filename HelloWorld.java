691eeba380a28b868f210847a82360b7e14ddd9b
Jobs <josuebatista.ti@gmail.com>
2019-01-30 15:38:02 -0300

    30.01.2019

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,19 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package olamundo;
+package helloworld;
 
 /**
  *
  * @author Administrador
  */
-public class OlaMundo {
+public class HelloWorld {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Olá Mundo!");
     }
     
 }
